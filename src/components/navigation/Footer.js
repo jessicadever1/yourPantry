ed4de3c00db81a6281904c2bab82796db47ca4e8
jessicadever1 <jessicadever1@gmail.com>
2022-05-23 10:48:20 -0500

    refactor footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -6,30 +6,42 @@ import { RiDoorOpenLine } from 'react-icons/ri'
 import './access.css'
 
 const Footer = () => {
+
+  const navMenuTabs = [
+    {
+      "name": 'Recipes',
+      "icon": <GiMeal color='#023E8A' className='navIcon' />,
+      "link": ''
+    },
+    {
+      "name": 'Pantry',
+      "icon": <RiDoorOpenLine color='#023E8A' className='navIcon' />,
+      "link": ''
+    },
+    {
+      "name": 'Meals',
+      "icon": <BsFillCalendar2DayFill color='#023E8A' className='navIcon' />,
+      "link": ''
+    },
+    {
+      "name": 'Shop',
+      "icon": <AiOutlineShoppingCart color='#023E8A' className='navIcon' />,
+      "link": ''
+    },
+  ]
+
   return (
     <>
-      <section className='row text topPad1 botPad1'>
-        <div className='grid25'>
-          <GiMeal color='#023E8A' className='navIcon' />
-          <div>Recipes</div>
-        </div>
-
-        <div className='grid25'>
-          <RiDoorOpenLine color='#023E8A' className='navIcon' />
-          <div>Pantry</div>
-        </div>
-
-        <div className='grid25'>
-          <BsFillCalendar2DayFill color='#023E8A' className='navIcon' />
-          <div>Meals</div>
-        </div>
-
-        <div className='grid25'>
-          <AiOutlineShoppingCart color='#023E8A' className='navIcon' />
-          <div>Shop</div>
-        </div>
-        
-      </section>
+      {navMenuTabs.map((option) => {
+        return (
+          <section className='row text topPad1 botPad1'>
+            <div className='grid25'>
+              <Link href={option.link}>{option.icon}</Link>
+              <div>{option.name}</div>
+            </div>
+        </section>
+        )
+      })}
     </>
   )
 }
